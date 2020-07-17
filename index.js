d7a8ab2dba5e4099b32c5d7645f75b8adb09b365
Meet Patel <patelmeet59@gmail.com>
2020-07-17 23:01:14 +0530

    refactor questions object

--- index.js
+++ index.js
@@ -2,72 +2,15 @@
 
 const program = require('commander'); // TODO: Make script more CLI friendly, provide description etc. using CommanderJS
 const { Input, Select, MultiSelect, NumberPrompt } = require('enquirer');
+const { questions } = require('./questions');
 
 // CommanderJS configuration for CLI flags and help menu
 program
   .version('1.0.0')
-  .description('A CLI tool to use pmm-framework and setup pmm-server, pmm-client with DBs instances locally or inside dedicated Vagrant boxes.');
-
-program.parse(process.argv);
+  .description('A CLI tool to use pmm-framework and setup pmm-server, pmm-client with DBs instances locally or inside dedicated Vagrant boxes.')
+  .parse(process.argv);
 
 let parameter_string = "./pmm-framework.sh --pmm2"; // Flags and parameters to be provided when pmm-framework.sh is executed
-
-// Questions list object
-const questions = {
-  q_operation_choice : {
-    name: 'pmm-server-client',
-    message: 'Select operation: ',
-    choices: ['Install pmm-server', 'Install pmm-client', 'Wipe all pmm configuration', 'Remove client at given IP', 'Remove server at given IP']
-  },
-  q_pmm_version : {
-    name: 'pmm-version',
-    message: 'Select PMM version to be installed: ',
-    choices: ['2.7.0', '2.6.0', 'Custom']
-  },
-  q_pmm_custom_version : {
-    message: 'Enter PMM version you want to install: ',
-    initial: '2.x.x'
-  },
-  q_dev_repo : {
-    name: 'dev-repo or not',
-    message: 'Do you want to install from development repository? ',
-    choices: ['Yes', 'No']
-  },
-  q_select_db : {
-    name: 'value',
-    message: 'Select DBs to install on client (Select using Space bar): ',
-    choices: [
-      { name: 'Percona Server (ps)', value: 'ps' },
-      { name: 'MySQL (ms)', value: 'ms' },
-      { name: 'MariaDB (md)', value: 'md' },
-      { name: 'Percona XtraDB Cluster (pxc)', value: 'pxc' },
-      { name: 'MongoDB (mo)', value: 'mo' },
-      { name: 'PostgreSQL (pgsql)', value: 'pgsql' }
-    ],
-    result(names) {   // This function returns just the short forms of DBs selected
-      let resultObject = this.map(names);
-      let result = [];
-      for(let key in resultObject){
-        result.push(resultObject[key]);
-      }
-      return result;
-    }
-  },
-  q_db_count: {
-    name: 'number',
-    message: 'Please tell number of instances: '
-  },
-  q_setup_vagrant : {
-    name: 'Install Vagrant or not',
-    message: 'Do you want to setup new Vagrant Box for configuration? ',
-    choices: ['Yes', 'No']
-  },
-  q_vagrant_os : {
-    name: 'Vagrant OS',
-    message: 'Select OS for the Vagrant Box: ',
-    choices: ['Ubuntu', 'CentOS']
-  }
-}
 
 
 // Main function
